2363bbb7687638242f721efc9309a08f9e402504
raoyongqi <1152416230@qq.com>
2025-01-08 10:20:29 +0800

    0108

--- clip_solve.py
+++ clip_solve.py
@@ -4,12 +4,28 @@ from rasterio.mask import mask
 import numpy as np
 import geopandas as gpd
 import platform
+
+
+import platform
+# 遍历 TIFF 文件夹列表中的每个文件夹
+
+
+from shapely.geometry import Polygon, MultiPolygon
+import matplotlib.pyplot as plt
+# 遍历每个 TIFF 文件所在的文件夹
+
+
+from shapely.geometry import Polygon
+
 if platform.system() == "Windows":
     # Windows 环境的文件路径
     base_path = r'C:\Users\r\Desktop\ssp'
 else:
     # Linux 环境的文件路径
-    base_path = '/home/r/Desktop/r-climate'
+    base_path = '/home/r/Desktop/ssp'
+
+
+
 geojson_file_path = '中华人民共和国分市.json'  # 请确保这个文件路径正确
 gdf_geojson = gpd.read_file(geojson_file_path)
 
@@ -18,11 +34,48 @@ import pandas as pd
 # Read the Excel file
 df = pd.read_csv('data/climate_soil_loc.csv')  # Replace with the path to your Excel file
 
+import pandas as pd
+
+
+# 将城市和省级信息映射到 geo_df 中
+# 假设你已经有了通过 `City` 和 `Province` 找到 `adcode` 的方法
+# 这里你可以利用某些匹配条件 (比如 name 和 Province 来匹配)
+
+
+
+cities_data = df[['City','Province','District']]
+
 # Get the unique cities from the "City" column
 unique_cities = df['City'].unique()
 
 filtered_gdf = gdf_geojson[gdf_geojson['name'].isin(unique_cities)]
-print(gdf_geojson['name'])
+
+
+
+
+# 检查并清理无效几何
+filtered_gdf = filtered_gdf[filtered_gdf.is_valid]
+
+
+
+merged_df = pd.merge(cities_data, filtered_gdf, left_on='City', right_on='name', how='inner')
+
+
+provinces_to_include = ['西藏自治区', '新疆维吾尔自治区', '甘肃省', '青海省', '四川省', '内蒙古自治区']
+
+# 过滤 merged_df，保留 Province 列在 provinces_to_include 列表中的数据
+filtered_merged_df = merged_df[merged_df['Province'].isin(provinces_to_include)]
+
+
+province_gdf =  gpd.read_file('中华人民共和国.json')
+
+excluded_provinces = ['西藏自治区', '新疆维吾尔自治区', '甘肃省', '青海省', '四川省', '内蒙古自治区']
+
+# 使用 `.loc` 和 `.isin()` 来筛选出不在 excluded_provinces 列表中的省份
+filtered_province_gdf = province_gdf[~province_gdf['name'].isin(excluded_provinces)]
+
+merged_gdf = gpd.GeoDataFrame( pd.concat([filtered_merged_df, filtered_province_gdf], ignore_index=True))
+
 
 # from shapely import MultiPolygon
 
@@ -51,20 +104,7 @@ tiff_output_folder = os.path.join(base_path, 'data', 'cropped_result', 'tiff')
 os.makedirs(geojson_output_folder, exist_ok=True)
 os.makedirs(tiff_output_folder, exist_ok=True)
 
-# 遍历 TIFF 文件夹列表中的每个文件夹
-import os
-import rasterio
-import numpy as np
-from rasterio.mask import mask
-from shapely.geometry import Polygon, MultiPolygon
 
-# 遍历每个 TIFF 文件所在的文件夹
-import os
-import rasterio
-import numpy as np
-from rasterio.mask import mask
-from shapely.geometry import Polygon
-import geopandas as gpd
 
 # 假设已加载过滤后的GeoDataFrame（filtered_gdf）
 # 假设 filtered_gdf 已经被定义，并且其包含了一个 'geometry' 列（每个几何对象为 Polygon 或 MultiPolygon）
@@ -106,7 +146,7 @@ for tiff_folder in tiff_folders:
                             dataset.write(image_data, 1)
 
                             # 使用 GeoDataFrame 的几何对象进行剪切，并处理缺失值
-                            out_image, out_transform = mask(dataset, filtered_gdf.geometry, crop=True, nodata=np.nan)
+                            out_image, out_transform = mask(dataset, merged_gdf.geometry, crop=True, nodata=np.nan)
 
                             # 更新元数据
                             out_meta = dataset.meta.copy()
@@ -128,7 +168,7 @@ for tiff_folder in tiff_folders:
 
                 # 将 GeoJSON 中的值更新为裁剪后的 TIFF 中的值
                 with rasterio.open(tiff_output_path) as cropped_src:
-                    for idx, row in filtered_gdf.iterrows():
+                    for idx, row in merged_gdf.iterrows():
                         geom = row['geometry']
                         if geom.is_empty:
                             continue
@@ -157,6 +197,6 @@ for tiff_folder in tiff_folders:
                         # 计算平均值
                         if all_pixel_values:
                             avg_value = np.nanmean(all_pixel_values)  # 使用平均值来代表区域
-                            filtered_gdf.at[idx, 'value'] = avg_value
+                            merged_gdf.at[idx, 'value'] = avg_value
                         else:
-                            filtered_gdf.at[idx, 'value'] = np.nan  # 如果没有有效值，则设置为 NaN
+                            merged_gdf.at[idx, 'value'] = np.nan  # 如果没有有效值，则设置为 NaN
